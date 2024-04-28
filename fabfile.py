fc93b46fd311f219940a57e56130ce1d75b97f39
rishabh <rishabhsingh@playsimple.in>
2024-04-28 16:30:26 +0530

    created fabfile

---
+++ fabfile.py
@@ -0,0 +1,15 @@
+from fabric.api import local
+
+def test():
+    print("Hello test")
+
+def commit():
+    print("Hello commit")
+
+def push():
+    print("Hello push")
+
+def prepare_deploy():
+    test()
+    commit()
+    push()
