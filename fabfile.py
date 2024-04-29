800fe113c12e9e5913307e02820a945183ca5b43
rishabh <rishabhsingh@playsimple.in>
2024-04-29 11:29:21 +0530

    added ssh connection code

--- fabfile.py
+++ fabfile.py
@@ -3,6 +3,15 @@ import os
 
 from fabric import Connection, task
 
+def read_config():
+    path = os.environ['SERVERS_CONFIG']
+    f = open(path, 'r')
+    obj = json.loads(f.read())
+    return obj
+
 @task
 def run_docker(ctx):
-   print("deploy_prod")
+   config = read_config()
+   hosts = config["host_names"]
+   for host in hosts:
+    c = Connection(host, connect_timeout=60)
