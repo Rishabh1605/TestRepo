c868bcd07181dfbcbb6e7fd1f90b2ec84a387f29
rishabh <rishabhsingh@playsimple.in>
2024-04-29 10:41:33 +0530

    removed unused arguments

--- __init__.py
+++ __init__.py
@@ -4,9 +4,9 @@ import os
 from fabric import Connection, task
 
 @task
-def deploy_prod(ctx):
+def deploy_prod():
    print("deploy_prod")
 
 @task
-def deploy_staging(ctx):
+def deploy_staging():
    print("deploy_staging")
